1696a219423dfc4aaf470d5e5a9562a294a7c8a2
Lorenz Schori <lo@znerol.ch>
2011-10-11 10:07:13 +0000

    Get rid of Dpath.xl and getSnakeStart()

--- test/editgraphTest.js
+++ test/editgraphTest.js
@@ -1,86 +1,6 @@
 (function(exports, editgraph) {
 
     /**
-     * Test simple edit graph algorithm. Expected graph is taken from the example
-     * in myers paper on page 7.
-     */
-    exports.testSimple = function(test) {
-        var expect_downs = [
-            // d=0
-            [ 0, 0 ],
-            // d=1
-            [ 0, 1 ],
-            // d=2
-            [ 0, 2 ],
-            [ 1, 1 ],
-            // d=3
-            [ 2, 5 ],
-            [ 3, 2 ],
-            // d=4
-            [ 3, 7 ],
-            [ 4, 6 ],
-            [ 5, 5 ],
-            // d=5
-            [ 3, 8 ],
-            [ 4, 7 ],
-            [ 5, 6 ],
-            [ 7, 6 ]
-        ];
-        var expect_rights = [
-            // d=1
-            [1, 0],
-            // d=2
-            [2, 0],
-            // d=3
-            [3, 4],
-            [4, 1],
-            // d=4
-            [6, 4],
-            [6, 2],
-            ];
-        var expect_diags = [
-            // d=2
-            [1, 3],
-            [2, 4],
-            [2, 2],
-            [3, 1],
-            // d=3
-            [3, 6],
-            [4, 5],
-            [4, 3],
-            [5, 4],
-            [5, 2],
-            // d=4
-            [7, 5],
-            [7, 3],
-            ];
-        var actual_downs = [];
-        var actual_rights = [];
-        var actual_diags = [];
-
-        var builder = {
-            'down': function(x, y) {
-                actual_downs.push([x, y]);
-            },
-            'right': function(x, y) {
-                actual_rights.push([x, y]);
-            },
-            'diag': function(x, y) {
-                actual_diags.push([x, y]);
-            }
-        };
-
-        var graph = new editgraph.Editgraph('abcabba', 'cbabac');
-        var d = graph.editgraph_simple(builder);
-
-        test.equal(d, 5);
-        test.deepEqual(actual_downs, expect_downs);
-        test.deepEqual(actual_rights, expect_rights);
-        test.deepEqual(actual_diags, expect_diags);
-        test.done();
-    };
-
-    /**
      * Test simple lcs algorithm. Expected graph is taken from the example in myers
      * paper on page 3.
      */
@@ -128,9 +48,9 @@
         ];
         var actual_common_symbols = [];
 
-        p1 = new editgraph.Dpath(0, 0, 0);
-        p2 = new editgraph.Dpath(-1, 0, 0);
-        p3 = new editgraph.Dpath(-2, 0, 2);
+        p1 = new editgraph.Dpath(0, 0);
+        p2 = new editgraph.Dpath(-1, 0);
+        p3 = new editgraph.Dpath(-2, 2);
 
         [[p1, p2], [p2, p3]].forEach(function(pair) {
             editgraph.Dpath.forEachCommonSymbolForward(pair[0], pair[1], function(x, y) {
@@ -149,9 +69,9 @@
         ];
         var actual_common_symbols = [];
 
-        p1 = new editgraph.Dpath(0, 0, 0);
-        p2 = new editgraph.Dpath(-1, 0, 0);
-        p3 = new editgraph.Dpath(-2, 0, 2);
+        p1 = new editgraph.Dpath(0, 0);
+        p2 = new editgraph.Dpath(-1, 0);
+        p3 = new editgraph.Dpath(-2, 2);
 
         [[p1, p2], [p2, p3]].forEach(function(pair) {
             editgraph.Dpath.forEachCommonSymbolBackward(pair[0], pair[1], function(x, y) {
@@ -170,9 +90,9 @@
         ];
         var actual_common_symbols = [];
 
-        p1 = new editgraph.Dpath(1, 7, 7);
-        p2 = new editgraph.Dpath(2, 5, 7);
-        p3 = new editgraph.Dpath(1, 3, 4);
+        p1 = new editgraph.Dpath(1, 7);
+        p2 = new editgraph.Dpath(2, 7);
+        p3 = new editgraph.Dpath(1, 4);
 
         [[p3, p2], [p2, p1]].forEach(function(pair) {
             editgraph.Dpath.forEachCommonSymbolForward(pair[0], pair[1], function(x, y) {
@@ -191,9 +111,9 @@
         ];
         var actual_common_symbols = [];
 
-        p1 = new editgraph.Dpath(1, 7, 7);
-        p2 = new editgraph.Dpath(2, 5, 7);
-        p3 = new editgraph.Dpath(1, 3, 4);
+        p1 = new editgraph.Dpath(1, 7);
+        p2 = new editgraph.Dpath(2, 7);
+        p3 = new editgraph.Dpath(1, 4);
 
         [[p2, p1], [p3, p2]].forEach(function(pair) {
             editgraph.Dpath.forEachCommonSymbolBackward(pair[0], pair[1], function(x, y) {
