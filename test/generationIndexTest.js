33cb9d7379f5c5e0baf32f72c9aab92af86f93f9
Lorenz Schori <lo@znerol.ch>
2011-11-13 22:16:38 +0000

    Introduce GenerationIndex.first/last

--- test/generationIndexTest.js
+++ test/generationIndexTest.js
@@ -151,6 +151,74 @@
 
         test.done();
     }
+
+    exports['should return first and last node of a specified generation'] = function(test) {
+        var a = new tree.Node();
+        var a1 = new tree.Node();
+        var a11 = new tree.Node();
+        var a2 = new tree.Node();
+        var a21 = new tree.Node();
+        var a22 = new tree.Node();
+        var g = new tree.GenerationIndex(a);
+
+        a.append(a1);
+        a1.append(a11);
+        a.append(a2);
+        a2.append(a21);
+        a2.append(a22);
+
+        g.buildAll();
+
+        // Depth = 0
+        result = g.first(0);
+        test.equals(result, a);
+
+        result = g.last(0);
+        test.equals(result, a);
+
+        // Depth = 1
+        result = g.first(1);
+        test.equals(result, a1);
+
+        result = g.last(1);
+        test.equals(result, a2);
+
+        // Depth = 2
+        result = g.first(2);
+        test.equals(result, a11);
+
+        result = g.last(2);
+        test.equals(result, a22);
+
+        test.done();
+    }
+
+    exports['should return undefined for first and last if depth is out of bounds'] = function(test) {
+        var a = new tree.Node();
+        var a1 = new tree.Node();
+        var a11 = new tree.Node();
+        var a2 = new tree.Node();
+        var a21 = new tree.Node();
+        var a22 = new tree.Node();
+        var g = new tree.GenerationIndex(a);
+
+        a.append(a1);
+        a1.append(a11);
+        a.append(a2);
+        a2.append(a21);
+        a2.append(a22);
+
+        g.buildAll();
+
+        // Depth = 3
+        result = g.first(3);
+        test.equals(result, undefined);
+
+        result = g.last(3);
+        test.equals(result, undefined);
+
+        test.done();
+    }
 }(
     typeof exports === 'undefined' ? (DeltaJS.generationIndexTest={}) : exports,
     typeof require === 'undefined' ? DeltaJS.tree : require('deltajs').tree
