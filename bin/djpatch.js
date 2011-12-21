456d92197c9b92479404429e003ad476c52d43b8
Lorenz Schori <lo@znerol.ch>
2011-12-21 08:19:28 +0000

    Rewrite djpatch

--- bin/djpatch.js
+++ bin/djpatch.js
@@ -5,7 +5,8 @@ var sys = require('sys');
 var fs  = require('fs');
 var path = require('path');
 var mime = require('mime');
-var deltajs = require('../lib/main');
+var patch = require('../lib/delta/patch');
+var profiles = require('../lib/profiles');
 
 
 /**
@@ -25,151 +26,6 @@ function checkfile(description, filepath, wantmime) {
         process.exit(1);
     }
     return filemime;
-}
-
-
-/**
- * Parses and loads a file using the given adapter classes
- */
-function loadFile(description, filepath, encoding, payloadhandler, dataadapter) {
-    var data, doc, tree;
-    data = fs.readFileSync(filepath, encoding);
-    doc = payloadhandler.parseString(data);
-    tree = dataadapter.adaptDocument(doc);
-    return tree;
-}
-
-
-/**
- * Save data to a file using the given adapter and payload handler
- */
-function saveFile(description, filepath, encoding, data, doc, payloadhandler, docadapter) {
-    var buf;
-    docadapter.createDocument(doc, data);
-    buf = payloadhandler.serializeToString(doc);
-    fs.writeFileSync(filepath, buf, encoding);
-}
-
-
-/**
- * Write serialized data to stdout
- */
-function showFile(description, data, doc, payloadhandler, docadapter) {
-    var buf;
-    docadapter.createDocument(doc, data);
-    buf = payloadhandler.serializeToString(doc);
-    sys.puts(buf);
-}
-
-
-/**
- * Return the payload type for a given mimetype.
- */
-function getPayloadType(mimetype) {
-    if (mimetype === 'application/json') {
-        return 'json';
-    }
-    else if (mimetype === 'application/xml' || mimetype.slice(-4) === '+xml') {
-        return 'xml';
-    }
-}
-
-
-/**
- * Return the proper payload handler for the given mime type. Return undefined
- * if no suitable payload handler is available.
- */
-function createPayloadHandler(type) {
-    var result;
-
-    switch(type) {
-        case 'json':
-            result = new deltajs.jsonpayload.JSONPayloadHandler();
-            break;
-        case 'xml':
-            result = new deltajs.xmlpayload.XMLPayloadHandler();
-            break;
-    }
-
-    return result;
-}
-
-
-/**
- * Return the proper tree adapter for this payload type.
- */
-function createTreeAdapter(type) {
-    var result;
-
-    switch(type) {
-        case 'json':
-            result = new deltajs.jsobjecttree.JSObjectTreeAdapter();
-            break;
-        case 'xml':
-            result = new deltajs.domtree.DOMTreeAdapter();
-            break;
-    }
-
-    return result;
-}
-
-
-/**
- * Return the proper tree adapter for this payload type.
- */
-function createDeltaAdapter(type, fragadapter) {
-    var result;
-
-    switch (type) {
-        case 'json':
-            result = new deltajs.jsondelta.JSONDeltaAdapter(fragadapter);
-            break;
-        case 'xml':
-            result = new deltajs.domdelta.DOMDeltaAdapter(fragadapter);
-            break;
-    }
-
-    return result;
-}
-
-/**
- * Return proper tree value index for payload type.
- */
-function createValueIndex(type) {
-    var result;
-
-    switch (type) {
-        case 'xml':
-            result = new deltajs.tree.NodeHashIndex(
-                    new deltajs.domtree.DOMNodeHash(deltajs.fnv132.Hash));
-            break;
-        case 'json':
-            // no index
-            break;
-    }
-
-    return result;
-}
-
-
-/**
- * Return proper operation handler factory for payload type.
- */
-function createHandlerFactory(type) {
-    var result;
-
-    switch (type) {
-        case 'xml':
-            result = new deltajs.domdelta.DOMOperationHandlerFactory();
-            break;
-        case 'json':
-            // no index
-            console.error('JSON patch handler factory for not implemented yet');
-            sys.exit(1);
-            break;
-    }
-
-    return result;
 }
 
 
@@ -227,101 +83,62 @@ function main() {
 
 
     // Check input files
-    var documentMimetype, documentPayloadType, documentPayloadHandler,
-        documentTreeAdapter;
+    var documentMimetype, patchMimetype, documentPayloadType, patchPayloadType,
+        resolverProfile, documentProfile, deltaProfile;
 
-    documentMimetype = checkfile('original file', options.origfile,
-            documentMimetype);
+    if (!options.filetype) {
+        documentMimetype = checkfile('original file', options.origfile);
 
-    // Setup document payload handler and tree adapter
-    documentPayloadType = getPayloadType(documentMimetype);
-    if (!documentPayloadType) {
-        console.error('This file type is not supported by djdiff');
+        // Setup document payload handler and tree adapter
+        documentPayloadType = profiles.getPayloadType(documentMimetype);
+        if (!documentPayloadType) {
+            console.error('This file type is not supported by djdiff');
+            process.exit(1);
+        }
+    }
+    else {
+        documentPayloadType = options.filetype;
     }
 
-    documentPayloadHandler = createPayloadHandler(documentPayloadType);
-    if (!documentPayloadHandler) {
-        console.error('This file type is not supported by djdiff');
-    }
-
-    documentTreeAdapter = createTreeAdapter(documentPayloadType);
-
-
-    // Check patch file
-    var patchMimetype, patchPayloadType, patchPayloadHandler, patchAdapter;
     patchMimetype = checkfile('patch file', options.patchfile);
+    patchPayloadType = profiles.getPayloadType(patchMimetype);
 
 
-    // Setup patch payload handler and tree adapter
-    patchPayloadType = getPayloadType(patchMimetype);
-    if (!patchPayloadType) {
-        console.error('This patch type is not supported by djdiff');
+    // Setup algorithm profile
+    resolverProfile = profiles.getResolverProfile();
+    if (!resolverProfile) {
+        console.error('The specified patch algorithm is not supported');
+        process.exit(1);
     }
 
-    patchPayloadHandler = createPayloadHandler(patchPayloadType);
-    if (!patchPayloadHandler) {
-        console.error('This patch type is not supported by djdiff');
+    // Setup input profile
+    documentProfile = profiles.getDocumentProfile(documentPayloadType);
+    if (!documentProfile) {
+        console.error('The file type "' + documentPayloadType + '" is not supported by djdiff');
+        process.exit(1);
     }
 
-
-    // Read input
-    var data, doc, tree;
-    data = fs.readFileSync(options.origfile, options.origenc);
-    doc = documentPayloadHandler.parseString(data);
-    tree = documentTreeAdapter.adaptDocument(doc);
-
-    nodevalidx = createValueIndex(documentPayloadType);
-    treevalidx = new deltajs.tree.TreeHashIndex(
-            new deltajs.tree.SimpleTreeHash(deltajs.fnv132.Hash, nodevalidx));
-
-    // Read patchfile
-    var patch, fragadapter, deltaAdapter;
-    fragadapter = documentPayloadHandler.createTreeFragmentAdapter(doc,
-            documentTreeAdapter, patchPayloadType);
-    deltaAdapter = createDeltaAdapter(patchPayloadType, fragadapter);
-
-    patch = loadFile('patch file', options.patchfile, options.patchenc,
-            patchPayloadHandler, deltaAdapter);
-
-    // Resolve operations
-    a_index = new deltajs.tree.DocumentOrderIndex(tree);
-    a_index.buildAll();
-
-    var resolver = new deltajs.resolver.ContextResolver(tree, a_index,
-            options.radius, options.threshold);
-
-    resolver.equalContent = function(docnode, patchnode, type) {
-        if (type === deltajs.delta.UPDATE_FOREST_TYPE) {
-            return treevalidx.get(docnode) === treevalidx.get(patchnode);
-        }
-        else if (type === deltajs.delta.UPDATE_NODE_TYPE) {
-            return nodevalidx.get(docnode) === nodevalidx.get(patchnode);
-        }
-        else {
-            throw new Error('Got unknown operation type in equalContent cb: ' + type);
-        }
+    // Setup delta profile
+    deltaProfile = profiles.getDeltaProfile(patchPayloadType);
+    if (!deltaProfile) {
+        console.error('The patch type "' + patchPayloadType + '" is not supported by djdiff');
+        process.exit(1);
     }
 
-    resolver.equalContext = function(docnode, value) {
-        return nodevalidx.get(docnode) === value;
-    }
+    // Read input files
+    var doc = documentProfile.loadOriginalDocument(
+            fs.readFileSync(options.origfile, options.origenc),
+            options.origfile);
+    var fragadapter = documentProfile.createFragmentAdapter(patchPayloadType);
+    var deltadoc = deltaProfile.loadDocument(
+            fs.readFileSync(options.patchfile, options.patchenc),
+            options.patchfile, fragadapter);
 
-    var handlerfactory = createHandlerFactory(documentPayloadType);
-    var fails = patch.attach(resolver, handlerfactory);
-
-    // Apply patch
-    patch.forEach(function(op, handler) {
-        if (handler) {
-            handler.toggle();
-        }
-        else {
-            console.error('failed to resolve hunk');
-        }
-    });
+    var p = new patch.Patch(resolverProfile, documentProfile);
+    p.patch(doc, deltadoc);
 
     // Serialize tree
-    var buf = documentPayloadHandler.serializeToString(doc);
-    sys.puts(buf);
+    sys.puts(documentProfile.payloadHandler.serializeToString(doc.data));
 }
 
 main();
