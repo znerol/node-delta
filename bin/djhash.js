c65649de62bff6d48433f972ae465d55329fad01
Lorenz Schori <lo@znerol.ch>
2012-01-06 12:15:07 +0000

    UtiUtility displaying hash values for each node in an xml document

---
+++ bin/djhash.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+var optparse = require('optparse');
+var sys = require('sys');
+var fs  = require('fs');
+var profiles = require('../lib/profiles');
+
+function indent(level, s) {
+    var i, result = '';
+    s = s || '    ';
+    for (i = 0; i < level; i++) {
+        result += s;
+    }
+    return result;
+}
+
+function fill(line, pos, s) {
+    var i, result;
+    s = s || ' ';
+    result = line + s;
+    while(result.length < pos) {
+        result += s;
+    }
+    return result;
+}
+
+/**
+ * Parse options and command line arguments and initialize the diff algorithm
+ */
+function main() {
+    var options = {
+        'origfile': undefined,
+        'origenc': 'UTF-8',
+    }
+
+    var switches = [];
+
+    var parser = new optparse.OptionParser(switches);
+    parser.banner = 'Usage: djhash [options] FILE';
+
+    parser.on('help', function(name, value) {
+        sys.puts(parser.toString());
+    });
+
+    parser.on(2, function(value) {
+        options.origfile=value
+    });
+
+    parser.parse(process.ARGV);
+
+    // Setup input profile
+    var documentProfile = profiles.getDocumentProfile('xml');
+
+    // Read input files
+    var doc = documentProfile.loadOriginalDocument(
+            fs.readFileSync(options.origfile, options.origenc),
+            options.origfile);
+
+    var i = 0, node, line;
+    while ((node = doc.nodeindex.get(doc.tree, i))) {
+        line = indent(node.depth) + ' ' + node.value;
+
+        line = fill(line, 40);
+        line += doc.valueindex.get(node).toString(16);
+        line += " " + doc.treevalueindex.get(node).toString(16);
+        sys.puts(line);
+        i++;
+    }
+}
+
+main();
