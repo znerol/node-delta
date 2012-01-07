47fc464f9f57b9f7a0f603b5d91b880e29e81ce0
Lorenz Schori <lo@znerol.ch>
2012-01-07 22:53:30 +0000

    Change return type of top-down resolver to parent-index instead of array-of nodes structure

--- test/TopDownPathResolverTest.js
+++ test/TopDownPathResolverTest.js
@@ -2,12 +2,12 @@ var resolver = require('../lib/delta/resolver');
 var tree = require('../lib/delta/tree');
 
 // Setup
-var a = new tree.Node();
-var a1 = new tree.Node();
-var a11 = new tree.Node();
-var a2 = new tree.Node();
-var a21 = new tree.Node();
-var a22 = new tree.Node();
+var a = new tree.Node('a');
+var a1 = new tree.Node('a1');
+var a11 = new tree.Node('a11');
+var a2 = new tree.Node('a2');
+var a21 = new tree.Node('a21');
+var a22 = new tree.Node('a22');
 var r = new resolver.TopDownPathResolver(a);
 
 a.append(a1);
@@ -20,7 +20,7 @@ exports['should resolve to root node if path is empty'] = function(test) {
     var result;
 
     result = r.resolve([]);
-    test.deepEqual(result, [a]);
+    test.deepEqual(result, {'node':a, 'tail':[]});
 
     test.done();
 };
@@ -29,19 +29,19 @@ exports['should resolve to correct node if path exists'] = function(test) {
     var result;
 
     result = r.resolve([0]);
-    test.deepEqual(result, [a, a1]);
+    test.deepEqual(result, {'node': a, 'tail':[0]});
 
     result = r.resolve([0, 0]);
-    test.deepEqual(result, [a, a1, a11]);
+    test.deepEqual(result, {'node': a1, 'tail':[0]});
 
     result = r.resolve([1]);
-    test.deepEqual(result, [a, a2]);
+    test.deepEqual(result, {'node': a, 'tail':[1]});
 
     result = r.resolve([1, 0]);
-    test.deepEqual(result, [a, a2, a21]);
+    test.deepEqual(result, {'node': a2, 'tail':[0]});
 
     result = r.resolve([1, 1]);
-    test.deepEqual(result, [a, a2, a22]);
+    test.deepEqual(result, {'node': a2, 'tail':[1]});
 
     test.done();
 };
@@ -50,19 +50,28 @@ exports['should resolve to deepest matching node if path does not exist'] = func
     var result;
 
     result = r.resolve([-1]);
-    test.deepEqual(result, [a]);
+    test.deepEqual(result, {'node':a, 'tail':[-1]});
 
     result = r.resolve([2]);
-    test.deepEqual(result, [a]);
+    test.deepEqual(result, {'node':a, 'tail':[2]});
 
     result = r.resolve([0, -1]);
-    test.deepEqual(result, [a, a1]);
+    test.deepEqual(result, {'node': a1, 'tail':[-1]});
 
     result = r.resolve([0, 0, 1]);
-    test.deepEqual(result, [a, a1, a11]);
+    test.deepEqual(result, {'node': a11, 'tail':[1]});
 
     result = r.resolve([0, 1]);
-    test.deepEqual(result, [a, a1]);
+    test.deepEqual(result, {'node': a1, 'tail':[1]});
+
+    test.done();
+};
+
+exports['tail should contain the whole unresolvable path'] = function(test) {
+    var result;
+
+    result = r.resolve([0, 3, 5, 0, 1]);
+    test.deepEqual(result, {'node': a1, 'tail': [3, 5, 0, 1]});
 
     test.done();
 };
